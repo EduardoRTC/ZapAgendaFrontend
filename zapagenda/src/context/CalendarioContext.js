38a77fbe7e76f21bea0fb8b1fcc9f83bae13112d
Everson <sousekjuniorr@outlook.com>
2024-10-13 13:23:43 -0300

    feat: Altera funções do contexto para usar a lib date-fns

--- zapagenda/src/context/CalendarioContext.js
+++ zapagenda/src/context/CalendarioContext.js
@@ -1,35 +1,19 @@
 // CalendarioContext.jsx
-import React, { createContext, useEffect, useState } from 'react';
+import { format, isToday } from 'date-fns';
+import React, { createContext, useState } from 'react';
 
 export const CalendarioContext = createContext();
 
 export const CalendarioProvider = ({ children }) => {
   const [modoVisualizacao, setModoVisualizacao] = useState('mes'); // 'dia', 'semana', 'mes'
   const [dataSelecionada, setDataSelecionada] = useState(new Date());
-  const [dataFormatada, setDataFormatada] = useState('');
 
   // Novo estado para agendamentoSelecionado
   const [agendamentoSelecionado, definirAgendamentoSelecionado] = useState(null);
 
-  const formatarData = (data) => {
-    const ano = data.getFullYear();
-    const mes = String(data.getMonth() + 1).padStart(2, '0');
-    const dia = String(data.getDate()).padStart(2, '0');
-    return `${ano}-${mes}-${dia}`;
-  };
+  const formatarData = (data) => format(data, "yyyy-MM-dd");;
 
-  useEffect(() => {
-    setDataFormatada(formatarData(dataSelecionada));
-  }, [dataSelecionada]);
-
-  const eHoje = (data) => {
-    const hoje = new Date();
-    return (
-      data.getDate() === hoje.getDate() &&
-      data.getMonth() === hoje.getMonth() &&
-      data.getFullYear() === hoje.getFullYear()
-    );
-  };
+  const eHoje = (data) =>  isToday(data) ;
 
   const aoClicarDia = (data) => {
     setDataSelecionada(data);
@@ -46,7 +30,6 @@ export const CalendarioProvider = ({ children }) => {
     setModoVisualizacao,
     dataSelecionada,
     setDataSelecionada,
-    dataFormatada,
     formatarData,
     eHoje,
     aoClicarDia,
