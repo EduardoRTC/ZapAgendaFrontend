0e4b1f711b535498f38dce90506cc73fed93390f
EDUARDO ROCHA TOLEDO CARVALHO <eduardo.rtcarvalho@gmail.com>
2024-10-14 11:10:27 -0300

    Fix: corrige inconsistencias entre os nomes das propriedades

--- zapagenda/src/components/PopupAgendamento/PopupAgendamento.js
+++ zapagenda/src/components/PopupAgendamento/PopupAgendamento.js
@@ -1,20 +1,36 @@
 // PopupAgendamento.jsx
-import React from 'react';
-import './PopupAgendamento.css';
+import React from "react";
+import "./PopupAgendamento.css";
 
 const PopupAgendamento = ({ agendamento, aoFechar }) => {
   return (
     <div className="popup-agendamento">
       <div className="conteudo-popup">
         <h3>{agendamento.descricao}</h3>
-        <p><strong>Médico:</strong> {agendamento.medico}</p>
-        <p><strong>Paciente:</strong> {agendamento.paciente}</p>
-        <p><strong>Data:</strong> {agendamento.data}</p>
-        <p><strong>Horário:</strong> {agendamento.horario}</p>
-        <p><strong>Motivo:</strong> {agendamento.motivo}</p>
-        <p><strong>Plano de Saúde:</strong> {agendamento.planoSaude}</p>
-        <p><strong>Observações:</strong> {agendamento.observacoes}</p>
-        <button className="fechar-popup" onClick={aoFechar}>Fechar</button>
+        <p>
+          <strong>Médico:</strong> {agendamento.doutor}
+        </p>
+        <p>
+          <strong>Paciente:</strong> {agendamento.paciente}
+        </p>
+        <p>
+          <strong>Data:</strong> {agendamento.data}
+        </p>
+        <p>
+          <strong>Horário:</strong> {agendamento.horario}
+        </p>
+        <p>
+          <strong>Motivo:</strong> {agendamento.reason}
+        </p>
+        <p>
+          <strong>Plano de Saúde:</strong> {agendamento.planoDeSaude}
+        </p>
+        <p>
+          <strong>Observações:</strong> {agendamento.observations}
+        </p>
+        <button className="fechar-popup" onClick={aoFechar}>
+          Fechar
+        </button>
       </div>
     </div>
   );
