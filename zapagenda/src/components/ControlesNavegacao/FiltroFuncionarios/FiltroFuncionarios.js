357f30680cf12e7a16c294f54414ab4aa336419a
EDUARDO ROCHA TOLEDO CARVALHO <eduardo.rtcarvalho@gmail.com>
2024-10-15 10:37:15 -0300

    Fix: estiliza corretamente a combobox

--- zapagenda/src/components/ControlesNavegacao/FiltroFuncionarios/FiltroFuncionarios.js
+++ zapagenda/src/components/ControlesNavegacao/FiltroFuncionarios/FiltroFuncionarios.js
@@ -1,5 +1,4 @@
-// SelecaoFuncionario.jsx
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CalendarioContext } from "../../../context/CalendarioContext";
 import { AgendamentosContext } from "../../../context/AgendamentosContext";
 import "./FiltroFuncionarios.css";
@@ -9,25 +8,42 @@ const FiltroFuncionarios = () => {
     useContext(CalendarioContext);
   const { agendamentos } = useContext(AgendamentosContext);
 
+  const [isOpen, setIsOpen] = useState(false);
+
   // Extrair a lista de funcionários (doutores) dos agendamentos
   const listaFuncionarios = [
+    "Todos os funcionários",
     ...new Set(agendamentos.map((agendamento) => agendamento.doutor)),
   ];
 
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const handleSelect = (funcionario) => {
+    setFuncionarioSelecionado(
+      funcionario === "Todos os funcionários" ? null : funcionario
+    );
+    setIsOpen(false);
+  };
+
   return (
-    <div className="selecao-funcionario">
-      <select
-        id="funcionario-select"
-        value={funcionarioSelecionado || ""}
-        onChange={(e) => setFuncionarioSelecionado(e.target.value || null)}
-      >
-        <option value="">Todos os funcionários</option>
-        {listaFuncionarios.map((funcionario, index) => (
-          <option key={index} value={funcionario}>
-            {funcionario}
-          </option>
-        ))}
-      </select>
+    <div className="dropdown">
+      <div className="dropdown-select" onClick={toggleDropdown}>
+        {funcionarioSelecionado || "Todos os funcionários"}
+        <span className={`dropdown-seta ${isOpen ? "open" : ""}`}>&#9660;</span>
+      </div>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          {listaFuncionarios.map((funcionario, index) => (
+            <li
+              key={index}
+              className="dropdown-item"
+              onClick={() => handleSelect(funcionario)}
+            >
+              {funcionario}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
